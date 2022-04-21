eb66c595226eab478adfa2e445b1223b2bbb47cd
patryk_s <boydmalvo@gmail.com>
2022-04-21 19:04:53 +0200

    ORM

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.urls import path
 
-# Register your models here.
+from viewer.models import Genre, Movie
+from viewer.views import hello
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello/<s>', hello)
+]
--- viewer/models.py
+++ viewer/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, ForeignKey, DO_NOTHING, IntegerField, DateField, TextField, DateTimeField
 
 
 class Genre(Model):
     name = CharField(max_length=128)
+
+
+class Movie(Model):
+    title = CharField(max_length=128)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+    rating = IntegerField()
+    released = DateField()
+    description = TextField()
+    created = DateTimeField(auto_now_add=True)
